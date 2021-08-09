410407d949731a24ab127c75540e6a4a10a7a053
Badr Choujai <57566300+BadrChoujai@users.noreply.github.com>
2021-08-09 22:34:09 +0100

    Added the needed Vue components
    added vue components
    added vue dependancies needed fro now
    fixed foreign keys in migrations

--- Prod-active/app/Models/Category.php
+++ Prod-active/app/Models/Category.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Product;
 
 class Category extends Model
 {
     use HasFactory;
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+    }
+    protected $fillible = [
+        'name',
+    ];
 }
--- Prod-active/app/Models/Product.php
+++ Prod-active/app/Models/Product.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Category;
 class Product extends Model
 {
     use HasFactory;
+    public function category()
+    {
+        return $this->belongesTo(Category::class);
+    }
+    protected $fillible = [
+        'name',
+        'description',
+        'price',
+        'image',
+        'category_id'
+    ];
 }
