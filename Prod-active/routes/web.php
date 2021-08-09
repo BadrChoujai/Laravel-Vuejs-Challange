86db65a668b8a465f3082081fcf4187db103b927
Badr Choujai <badre-101@outlook.com>
2021-08-09 20:03:30 +0100

    Added controllers for product and category
    added controllers for product and category
    added models for the controllers
    and their migrations

--- Prod-active/routes/web.php
+++ Prod-active/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('api')->group(function (){
+    
+    // getting Products
+    Route::get('getProducts', 'ProductController@getProducts');
+    // getting Categories
+    Route::get('getCategories', 'CategoryController@getCategories'); 
+});
